8acafe6b2377e29a23c83d0b3c59e7b2baaab2ab
faizah115 <60900123037@uin-alauddin.ac.id>
2026-07-09 02:00:35 +0800

    fix: hapus provider yang tidak ada dari config/app.php

--- config/app.php
+++ config/app.php
@@ -107,9 +107,5 @@ return [
 
         // Application Service Providers...
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
     ],
 ];
